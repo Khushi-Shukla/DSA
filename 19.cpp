7a8a83b3eb1a9b04e4c23d6236d3810890373a36
unknown <khushishukla0502@gmail.com>
2022-09-22 00:06:59 +0530

    Linked list

--- 19.cpp
+++ 19.cpp
@@ -1,19 +1,16 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
 class Solution {
 public:
     ListNode* reverse(ListNode* head){
         ListNode* temp=head;
-        ListNode* prev=NULL, *nextone=NULL;
-        while(temp!=NULL){
+        ListNode* prev=nullptr, *nextone=nullptr;
+        while(temp!=nullptr){
             nextone=temp->next;
             temp->next=prev;
             prev=temp;
@@ -31,16 +28,43 @@ public:
             head=head->next;
             return reverse(head);
         }
-        while(temp!=NULL && temp->next!=NULL){
+        while(temp!=nullptr && temp->next!=nullptr){
             count++;
             if(count==n-1){
-                if(temp->next->next!=NULL)
+                if(temp->next->next!=nullptr)
                     temp->next=temp->next->next;
                 else
-                    temp->next=NULL;
+                    temp->next=nullptr;
             }
             temp=temp->next;
         }
         return reverse(head);
     }
 };
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    Solution s;
+    ListNode* head=new ListNode(1);
+    head->next=new ListNode(2);
+    head->next->next=new ListNode(3);
+    head->next->next->next=new ListNode(4);
+    head->next->next->next->next=new ListNode(5);
+
+    ListNode* temp=head;
+    while(temp!=nullptr){
+        cout<<temp->val<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+
+    s.removeNthFromEnd(head, 4);
+    temp=head;
+    while(temp!=nullptr){
+        cout<<temp->val<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
