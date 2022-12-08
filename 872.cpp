038143f6ec1fcd10e28872ebd0b009dc6dd116ac
unknown <khushishukla0502@gmail.com>
2022-12-08 23:27:48 +0530

    Leaf-Similar Trees

---
+++ 872.cpp
@@ -0,0 +1,30 @@
+// Leaf-Similar Trees
+#include<bits/stdc++.h>
+using namespace std;
+// Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    void traverse(TreeNode* root, vector<int> &v){
+        if(!root) return;
+        if(!root->left && !root->right){
+            v.push_back(root->val);
+        }
+        traverse(root->left, v);
+        traverse(root->right, v);
+    }
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> v1,v2;
+        traverse(root1, v1);
+        traverse(root2, v2);
+        return v1==v2;
+    }
+};
