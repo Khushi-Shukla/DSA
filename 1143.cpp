2a4c2a3b4d5cf611bc82cce91ca35c4165278d2c
unknown <khushishukla0502@gmail.com>
2022-12-15 22:59:43 +0530

    Longest Common Subsequence

---
+++ 1143.cpp
@@ -0,0 +1,22 @@
+// Longest Common Subsequence
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        if(text1==text2)
+            return text1.size();
+        int n=text1.size(), m=text2.size();
+        vector<vector<int>>dp(n+1, vector<int>(m+1));
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(text1[i-1]==text2[j-1])
+                    dp[i][j]=dp[i-1][j-1]+1;
+                else
+                    dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
+            }
+          
+        }
+          return dp[n][m];
+    }
+};
