cf2478d2e42c8fee4f69e78c4a708ed1d997e7fd
unknown <khushishukla0502@gmail.com>
2022-12-26 22:56:53 +0530

    Jump Game

---
+++ 55.cpp
@@ -0,0 +1,14 @@
+// Jump Game
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool canJump(vector<int>& a) {
+        int i,n=a.size(),maxi=0;
+        if(n==1) return 1;
+        for(i=0;i<n;i++){
+            if(maxi>=i) maxi=max(maxi, i+a[i]);
+        }
+        return maxi>=(n-1);
+    }
+};
