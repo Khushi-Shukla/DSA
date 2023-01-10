8738b4582a4d7010335fb97bd4c5c36087188db8
unknown <khushishukla0502@gmail.com>
2023-01-10 22:00:36 +0530

    Same Tree

---
+++ 100.cpp
@@ -0,0 +1,28 @@
+//Same Tree
+#include<bits/stdc++.h>
+using namespace std;
+ // Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL && q==NULL)
+            return true;
+        if(p==NULL || q==NULL)
+            return false;
+        bool cond1=p->val==q->val;
+        bool cond2=isSameTree(p->left, q->left);
+        bool cond3=isSameTree(p->right, q->right);
+        if(cond1 && cond2 && cond3)
+            return true;
+        return false;
+    }
+};
