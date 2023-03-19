3b9810288f506d3d309c942b5e393ae42ad67084
unknown <khushishukla0502@gmail.com>
2023-03-19 23:22:03 +0530

    Verify Preorder Serialization of a Binary Tree

---
+++ 331.cpp
@@ -0,0 +1,17 @@
+// Verify Preorder Serialization of a Binary Tree
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+        stringstream ss(preorder);
+        string curr;
+        int nodes = 1;
+        while (getline(ss, curr, ',')) {
+            nodes--;
+            if (nodes < 0) return false;
+            if (curr != "#") nodes += 2;
+        }
+        return nodes == 0;
+    }
+};
